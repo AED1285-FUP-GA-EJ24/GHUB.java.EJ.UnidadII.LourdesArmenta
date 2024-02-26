525d357d1f4048264893bfb6897302f5f15be035
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-02-26 14:39:11 +0000

    Actualizacion Hola Mundo

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,10 +1,9 @@
 package miPrincipal;
-
-
+//Comentario
 
 public class Principal {
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo, soy Lourdes"); //Esto es un comentario
     }
 }
